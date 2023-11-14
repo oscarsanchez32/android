bbec8715c881dd2f0c0bc24c8ace527f0e35b869
oscarsanchez32 <123061533+oscarsanchez32@users.noreply.github.com>
2023-11-14 19:31:24 +0100

    Add files via upload

---
+++ Contactos_EXT/app/src/main/java/com/example/contactos_ext/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.contactos_ext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
